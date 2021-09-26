9b04f36c83768a27e411c5ab98d91f37c083cdf6
Parizhanka <63254458+Parizhanka@users.noreply.github.com>
2021-09-26 20:32:11 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -3,7 +3,15 @@
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
-# TODO: your code here
+i = 1
+list_names = ""
+while i <= len(names):
+    if i == 1:
+        list_names = names[i-1]
+    else:
+        list_names = list_names + ", " + names[i-1]
+    i += 1
+print(list_names)
 
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
